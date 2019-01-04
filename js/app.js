35f9c68d81f49e2c5ed0d8f993fa8da534621212
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-04 12:43:40 +0000

    added ids to all the rows

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   for(let i = 0; i < 8; i++) {
     const newColumn = document.createElement('div')
     newColumn.classList.add('column')
+    newColumn.setAttribute('id', i)
     gameBoard.append(newColumn)
   }
 
@@ -13,8 +14,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     for(let i = 0; i < 8; i++) {
       const newRow = document.createElement('div')
       newRow.classList.add('row')
+      newRow.setAttribute('id', i)
       column.append(newRow)
-      console.log(newRow)
     }
   })
 })
