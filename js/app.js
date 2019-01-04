4ec212adad162260aeb3c250a8a3a6c520f76fe8
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-04 12:31:09 +0000

    created anitial grid

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  const h1 = document.querySelector('h1')
-  h1.innerText = 'heloo'
+  const gameBoard = document.querySelector('.game-board')
+
+  for(let i = 0; i < 8; i++) {
+    const newColumn = document.createElement('div')
+    newColumn.classList.add('column')
+    gameBoard.append(newColumn)
+  }
+
+  const columns = document.querySelectorAll('.column')
+
+  columns.forEach(column => {
+    for(let i = 0; i < 8; i++) {
+      const newRow = document.createElement('div')
+      newRow.classList.add('row')
+      column.append(newRow)
+      console.log(newRow)
+    }
+  })
 })
