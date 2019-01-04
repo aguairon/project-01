4fdb9ae246db31b99349e16d56b12a1a1a13bbef
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-04 14:03:17 +0000

    created dot and created a function to find a specific square

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-  const gameBoard = document.querySelector('.game-board')
+let gameBoard
+let columns
 
+function createBoard() {
   for(let i = 0; i < 8; i++) {
     const newColumn = document.createElement('div')
     newColumn.classList.add('column')
+    newColumn.classList.add(i)
     newColumn.setAttribute('id', i)
     gameBoard.append(newColumn)
   }
 
-  const columns = document.querySelectorAll('.column')
+  columns = document.querySelectorAll('.column')
 
   columns.forEach(column => {
     for(let i = 0; i < 8; i++) {
       const newRow = document.createElement('div')
       newRow.classList.add('row')
+      newRow.classList.add(i)
       newRow.setAttribute('id', i)
       column.append(newRow)
     }
   })
+}
+
+function findSquare(x, y) {
+  return document.querySelector('.column', `.${x}`).querySelector('.row', `.${y}`)
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  gameBoard = document.querySelector('.game-board')
+
+  createBoard()
+
+  const dot = document.createElement('span')
+  dot.classList.add('dot')
+  findSquare(0, 0).append(dot)
 })
