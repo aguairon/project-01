5daca47706610ba82fc0cddf23f14755a54f492f
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-04 14:57:15 +0000

    fixed the find square function and added the first 4 circles

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ function createBoard() {
   for(let i = 0; i < 8; i++) {
     const newColumn = document.createElement('div')
     newColumn.classList.add('column')
-    newColumn.classList.add(i)
-    newColumn.setAttribute('id', i)
+    newColumn.classList.add('column'+ i)
     gameBoard.append(newColumn)
   }
 
@@ -16,23 +15,30 @@ function createBoard() {
     for(let i = 0; i < 8; i++) {
       const newRow = document.createElement('div')
       newRow.classList.add('row')
-      newRow.classList.add(i)
-      newRow.setAttribute('id', i)
+      newRow.classList.add('row' + i)
       column.append(newRow)
     }
   })
+
+  addDisks(3, 3)
+  addDisks(3, 4)
+  addDisks(4, 4)
+  addDisks(4, 3)
+}
+
+function addDisks(x, y) {
+  const disk = document.createElement('span')
+  disk.classList.add('disk')
+  findSquare(x, y).append(disk)
 }
 
 function findSquare(x, y) {
-  return document.querySelector('.column', `.${x}`).querySelector('.row', `.${y}`)
+  const a = document.querySelector('.column'+ x).querySelector('.row' + y)
+  return a
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
   gameBoard = document.querySelector('.game-board')
 
   createBoard()
-
-  const dot = document.createElement('span')
-  dot.classList.add('dot')
-  findSquare(0, 0).append(dot)
 })
